23d14bedc398544db472f0872e6942f885bb4d08
Mathew Richards <richards.m32@gmail.com>
2020-05-12 12:58:16 -0600

    Continuing to work through. Need to manipulate the data and create proportion columns in the data table.

--- source_attribution.R
+++ source_attribution.R
@@ -1,6 +1,6 @@
 # Source attribution for Campy Isolates
 library(dplyr)
-
+library(ggplot2)
 # set a working directory (windows? - maybe switch to check linux)
 setwd("C:/Users/richa/Documents/R/source_attribution")
 getwd()
@@ -24,36 +24,35 @@ colnames(tdata)
 # also convert the df to a data table for better processing
 # ALSO the chance to use group_by
 
-# isolate a list of the subtype IDs
-sub <- data$Subtype.ID
-
-# remove any single values from this list - only subtypes with multiple results remain
-sub1 <- sub[duplicated(sub) | duplicated(sub, fromLast = TRUE)]
-
+# group by Subtype.ID
 gdata <- group_by(tdata, Subtype.ID)
 
-gdata
-
+# add a "count" column to filter out single IDs
 gdata_summary <- mutate(gdata,
-                        count = n(),
-                        unique = n_distinct(Source))
-gdata_summary
+                        count = n())
+                      #  distinct = n_distinct())
 
+# filter out the singular data points
 gdata_filtered <- filter(gdata_summary, count > 1)
+
+# create a column for proportional data based on count??
+gdata_filtered <- mutate(gdata_filtered,
+                         prop = 1) # need to group_by Source. Saving checkpoint in git)
 
 gdata_filtered
 
 fdata <- ungroup(gdata_filtered)
 
-View(fdata)
+colnames(fdata)
 
-# NOT WORKING tdata1 <- filter(tdata, Subtype.ID == sub1)
+# create data for each source as proportion of itself -> NO
 
-# maybe combine the commands?
+### need to plot the subtype ID as x, y is Source/count?
 
-# NOT WORKING tdata2 <- filter(tdata, Subtype.ID == sub[duplicated(sub) | duplicated(sub, fromLast = TRUE)])
+# g <- ggplot(fdata, aes(ID, Source, density))+geom_histogram(stat = "count")
+# g <- qplot(Subtype.ID, data = fdata, fill = Source)
+# g
 
-# create data for each source as proportion of itself
-# plot(x = data$Key, y = data$Source)
-# now work on ggplot2 to plot the data
+# better attempt, not ready yet
+## g <- ggplot(fdata, aes(x = Subtype.ID, y = prop, fill = source_contribution))
 

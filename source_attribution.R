60237e5ff1c45f888c3405fe3700139be11a4a1c
Mathew Richards <richards.m32@gmail.com>
2020-05-14 16:11:59 -0600

    Data manipulation should be done, now to clean up and work on the graphing portion

--- source_attribution.R
+++ source_attribution.R
@@ -6,55 +6,63 @@ library(tidyverse)
 setwd("C:/Users/richa/Documents/R/source_attribution")
 getwd()
 
-data <- read.csv("source_attribution_data.csv", fileEncoding = "UTF-8-BOM")
+raw_data <- read.csv("source_attribution_data.csv", fileEncoding = "UTF-8-BOM")
 
-tdata <- tbl_df(data)
+tbl_data <- tbl_df(raw_data)
 
-colnames(tdata)
-
-gdata <- group_by(tdata, Subtype.ID) %>%
+gdata <- group_by(tbl_data, Subtype.ID) %>%
   mutate(subtype_count = n()) %>%
-  filter(subtype_count > 4)
-
-gdata <- mutate(gdata, ratio_chicken = 0, ratio_human = 0,
+  filter(subtype_count > 4) %>%
+  mutate(ratio_chicken = 0, ratio_human = 0,
                  ratio_cattle = 0, ratio_water = 0)
-
-dim(gdata)[1]
-
-nrow(gdata)
 
 for (i in 1:nrow(gdata)) {
   if (gdata$Source[i] == "Chicken") {
     gdata$ratio_chicken[i] = 1/gdata$subtype_count[i]
-    
   } else if (gdata$Source[i] == "Human") {
     gdata$ratio_human[i] = 1/gdata$subtype_count[i]
-    
   } else if (gdata$Source[i] == "Cattle") {
     gdata$ratio_cattle[i] = 1/gdata$subtype_count[i]
-    
   } else if (gdata$Source[i] == "Water") {
     gdata$ratio_water[i] = 1/gdata$subtype_count[i]
-    
   } else {
-    print("Something is wrong, row", i)
+    print("ERROR, row", i)
   }
 }
 
-ggdata <- summarize(gdata, unique(Subtype.ID))
+gdata <- group_by(gdata, Subtype.ID)
+
+ggdata <- summarize(gdata, unique(Subtype.ID), sum(ratio_chicken),
+                    sum(ratio_human), sum(ratio_cattle), 
+                    sum(ratio_water))
+
+ggdata <- arrange(ggdata, desc(ggdata$`sum(ratio_chicken)`),
+                  desc(ggdata$`sum(ratio_human)`),
+                  desc(ggdata$`sum(ratio_cattle)`))
 
 View(ggdata)
 
-View(gdata)
+ratios <- c(ggdata$`sum(ratio_chicken)`, ggdata$`sum(ratio_human)`,
+            ggdata$`sum(ratio_cattle)`, ggdata$`sum(ratio_water)`)
 
-# sort according to proportion of chicken??
+sratios <- c(ggdata[,3:6])
 
+tratios <- select(ggdata, starts_with("sum(ratio"))
 
-# create data for each source as proportion of itself -> NO
+View(ratios) # list with length 4, each sub-list is 189
+View(sratios) # vector with 4*189 results
+View(tratios)
+
+g <- ggplot(ggdata, aes(`unique(Subtype.ID)`, tratios)) +
+  geom_bar(position = "fill", stat = "identity") +
+  ggtitle("Source Attribution Analysis") +
+  theme_gray()
+  
+g
+
 
 ### need to plot the subtype ID as x, y is Source/count?
 
-# g <- ggplot(fdata, aes(ID, Source, density))+geom_histogram(stat = "count")
 # g <- qplot(Subtype.ID, data = fdata, fill = Source)
 # g
 
